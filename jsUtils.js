d4a1f73e150ecfbf22338e05e34b58b79ab0b7d4
philipok <philipok-1@users.noreply.github.com>
2014-10-28 18:54:47 +0000

    Create jsUtils.js

---
+++ jsUtils.js
@@ -0,0 +1,21 @@
+function checkUpdate(){
+
+if (window.navigator.standalone){
+alert("web app")
+}
+/*applicationCache.update()	
+var appCache=window.applicationCache
+var message=""
+
+while (appCache!=1 or appCache!=0){
+	var appCache=window.applicationCache
+	if (appCache==2){alert("Checking for updates...")}
+	else if (appCache==3)(alert("Downloading update"))
+	}
+}
+	
+loadIntro();	
+}*/
+
+else {alert ("safari");}
+}

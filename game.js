8e9a767e7217cb371668b0530304b508253fd92e
philipok <philipok-1@users.noreply.github.com>
2014-10-29 11:01:18 +0000

    Update game.js

--- game.js
+++ game.js
@@ -4,27 +4,7 @@
 		document.getElementById('audiotag1').play();
 	}
 
-function checkUpdate(){
 
-if (window.navigator.standalone){
-alert("web app")
-}
-/*applicationCache.update()	
-var appCache=window.applicationCache
-var message=""
-
-while (appCache!=1 or appCache!=0){
-	var appCache=window.applicationCache
-	if (appCache==2){alert("Checking for updates...")}
-	else if (appCache==3)(alert("Downloading update"))
-	}
-}
-	
-loadIntro();	
-}*/
-
-else {alert ("safari");}
-}
 
 function loadIntro(){
 

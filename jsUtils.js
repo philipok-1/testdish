02a2802a6508e1c37fe8c431da1ea0228f05f94f
philipok <philipok-1@users.noreply.github.com>
2014-10-30 17:56:53 +0000

    Update jsUtils.js

--- jsUtils.js
+++ jsUtils.js
@@ -1,25 +1,11 @@
-//Application cache update function
 
-function checkUpdate(updatediv){
 
-window.applicationCache.update()	
-var appCache=window.applicationCache
-var checked=false
-
-while (checked==false){
-
-	document.getElementById(updatediv).style="visible"
-	document.getElementById(updatediv).innerHTML=appCache.status
-	if (appCache.status==4){checked=true}
-};
-
-alert ("finished");
-};
-
-function updateCache(){
+function onUpdateReady(){
 	
-	if (window.applicationCache==4){window.applicationCache.swapCache()
-		window.location.reload()
+	alert("found update")
+	
+	window.applicationCache.swapCache()
+	window.location.reload()
 		
 	}
-}
+

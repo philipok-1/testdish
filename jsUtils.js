e350c0df74c86c36156fcb31385daf1a8159b74e
philipok <philipok-1@users.noreply.github.com>
2014-10-31 09:10:44 +0000

    Update jsUtils.js

--- jsUtils.js
+++ jsUtils.js
@@ -6,13 +6,10 @@ window.applicationCache.addEventListener('updateready', onUpdateReady, false);}
  
 function onUpdateReady(){
 	
- if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-      
-      if (confirm('A new version of this site is available. Load it?')) {
+ 
+if (confirm('A new version of this site is available. Load it?')) {
         window.applicationCache.swapCache();
 window.location.reload();}
-else {loadIntro();}}
-
-else {loadIntro();}
+else {loadIntro()};
 
 }
